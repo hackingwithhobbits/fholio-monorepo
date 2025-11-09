7ad87a2437e06d2a8188ccf657ff5040510dabc9
Apoorv Mittal <dev@fholio.com>
2025-11-09 16:30:16 +0100

    removed package folder

--- packages/database/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Export everything
-export * from "./client";
-export * from "./queries/artists";
-export * from "./queries/snapshots";
-export * from "./queries/sync-logs";
-export * from "./types/database.types";
-export * from "./types/chartmetric.types";
